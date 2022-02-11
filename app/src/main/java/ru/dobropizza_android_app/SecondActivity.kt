61a0d24cca25fa8eb185833bf9a9cac80805cebc
rehkzylbz <rehkzybz@yandex.com>
2022-02-12 01:26:52 +0300

    webview added

---
+++ app/src/main/java/ru/dobropizza_android_app/SecondActivity.kt
@@ -0,0 +1,55 @@
+package ru.dobropizza_android_app
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (isOnline(this)) {
+            //загрузка сайта?
+            val webViewMain = WebView(this@SecondActivity)
+            webViewMain.webViewClient = WebViewClient()
+            webViewMain.settings.javaScriptEnabled = true
+            setContentView(webViewMain)
+            webViewMain.loadUrl("https://appi.dobropizza.ru")
+        }
+        else {
+            setContentView(R.layout.activity_main_not_online)
+        }
+    }
+
+    private fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            val capabilities =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                } else {
+                    null
+                }
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
